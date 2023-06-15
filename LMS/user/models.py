04e1d10938b23f18584ea94028eaf4ef0fec1eea
unknown <you@example.com>
2023-06-15 11:51:42 -0400

    update with user

--- LMS/user/models.py
+++ LMS/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Userprofile(models.Model):
+    name=models.CharField(max_length=30,default="abc")
+    image=models.ImageField()
+    date=models.DateField(auto_now=False, auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f'Hello {self.name} '
